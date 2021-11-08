cb7082c29cc9926c881080582aada6481efda9fe
Lenny Thach <lennythach1@gmail.com>
2021-11-08 13:16:28 -0800

    first commit

---
+++ website/auth.py
@@ -0,0 +1,68 @@
+from . import db
+from .models import User
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    # data = request.form
+    # print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        user = User.query.filter_by(email=email).first()
+
+
+        if len(email) < 3:
+            flash("Invalid email. must be more then 3 character long", category='error')
+        elif user:
+            flash('Email already exist! Please try again.', category='error')
+        elif len(first_name) < 2:
+            flash('Invalid first name must be greater then 1 character', category='error')
+        elif len(last_name) < 2:
+            flash('Invalid last name must be greater then 1 character', category='error')
+        elif password1 != password2:
+            flash('Password does not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            # login_user(user, remember=True)
+            flash('Account Created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("register.html", user=current_user)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password!', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
---
+++ website/models.py
@@ -0,0 +1,23 @@
+from datetime import timezone
+
+import sqlalchemy
+from sqlalchemy.orm import backref
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # Adding a foreign key to link the user with the note they are writing.
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    password = db.Column(db.String(150))
+    notes = db.relationship('Note')
